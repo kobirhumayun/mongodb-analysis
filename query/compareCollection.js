ca72dedf81aa0e2da59ccbe0ec0d0062d66a1032
Humayun <kobirhumayun@gmail.com>
2025-01-20 23:31:08 +0600

    feat: modify "compareCollections"
    Implement actual comparison logic
    of two collections

--- query/compareCollection.js
+++ query/compareCollection.js
@@ -2,23 +2,70 @@ const { runAggregation, findDocuments } = require('../helpers/dbQueryHelper');
 
 const compareCollections = async () => {
   try {
-    // Define aggregation pipelines or queries for two collections
+    // Define the database and collections
+    const dbName = "test_db";
+    const collection1Name = "data_1";
+    const collection2Name = "data_2";
+
+    // Define group and value fields for each collection
+    const collection1GroupField = "LCNo";
+    const collection1ValueField = "InvoiceValue";
+    const collection2GroupField = "LCNo";
+    const collection2ValueField = "InvoiceValue";
+
+    // Aggregation pipelines for each collection
     const pipeline1 = [
-      { $match: { /* criteria for collection1 */ } },
-      { $group: { /* aggregation logic for collection1 */ } }
+      {
+        $group: {
+          _id: `$${collection1GroupField}`,
+          totalValue: { $sum: `$${collection1ValueField}` }
+        }
+      }
     ];
+
     const pipeline2 = [
-      { $match: { /* criteria for collection2 */ } },
-      { $group: { /* aggregation logic for collection2 */ } }
+      {
+        $group: {
+          _id: `$${collection2GroupField}`,
+          totalValue: { $sum: `$${collection2ValueField}` }
+        }
+      }
     ];
 
     // Get data for both collections
-    const collection1Data = await runAggregation('collection1', pipeline1);
-    const collection2Data = await runAggregation('collection2', pipeline2);
+    const collection1Data = await runAggregation(dbName, 'data_1', pipeline1);
+    const collection2Data = await runAggregation(dbName, 'data_2', pipeline2);
 
     // Perform comparison logic on collection1Data and collection2Data
-    console.log('Collection 1 Data:', collection1Data);
-    console.log('Collection 2 Data:', collection2Data);
+    // Convert results to dictionaries for easy lookup
+    const results1Dict = Object.fromEntries(collection1Data.map(item => [item._id, item.totalValue]));
+    const results2Dict = Object.fromEntries(collection2Data.map(item => [item._id, item.totalValue]));
+
+    // Compare the results
+    const comparison = [];
+
+    const allKeys = new Set([...Object.keys(results1Dict), ...Object.keys(results2Dict)]);
+
+    allKeys.forEach(key => {
+      const value1 = results1Dict[key] || 0;
+      const value2 = results2Dict[key] || 0;
+      const difference = value1 - value2;
+
+      if (difference !== 0) {
+        comparison.push({
+          [collection1GroupField]: key,
+          [`${collection1Name}_total`]: value1,
+          [`${collection2Name}_total`]: value2,
+          difference: difference
+        });
+      }
+    });
+
+    // Print the comparison results
+    console.log("Comparison Results:");
+    comparison.forEach(item => {
+      console.log(JSON.stringify(item, null, 2));
+    });
     // Implement your comparison logic here...
 
   } catch (error) {
