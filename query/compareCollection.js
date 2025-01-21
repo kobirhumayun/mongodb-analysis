9072aa60bfde798f3b0fea993cbfa941cdb8f004
Humayun <kobirhumayun@gmail.com>
2025-01-21 17:14:53 +0600

    feat: call "compareData"

--- query/compareCollection.js
+++ query/compareCollection.js
@@ -1,6 +1,6 @@
 const { runAggregation, findDocuments } = require('../helpers/dbQueryHelper');
 const { connectDB, closeDB } = require('../config/db');
-
+const { compareData, filterObjectsByPathValue } = require('../utils/objectsComparision');
 
 const compareCollections = async () => {
   try {
@@ -38,30 +38,7 @@ const compareCollections = async () => {
     const collection1Data = await runAggregation(dbName, 'data_1', pipeline1);
     const collection2Data = await runAggregation(dbName, 'data_2', pipeline2);
 
-    // Perform comparison logic on collection1Data and collection2Data
-    // Convert results to dictionaries for easy lookup
-    const results1Dict = Object.fromEntries(collection1Data.map(item => [item._id, item.totalValue]));
-    const results2Dict = Object.fromEntries(collection2Data.map(item => [item._id, item.totalValue]));
-
-    // Compare the results
-    const comparison = [];
-
-    const allKeys = new Set([...Object.keys(results1Dict), ...Object.keys(results2Dict)]);
-
-    allKeys.forEach(key => {
-      const value1 = results1Dict[key] || 0;
-      const value2 = results2Dict[key] || 0;
-      const difference = value1 - value2;
-
-      if (difference !== 0) {
-        comparison.push({
-          [collection1GroupField]: key,
-          [`${collection1Name}_total`]: value1,
-          [`${collection2Name}_total`]: value2,
-          difference: difference
-        });
-      }
-    });
+    const comparison = compareData(collection1Data, collection2Data, collection1GroupField, collection1Name, collection2Name, "_id", "totalValue");
 
     // Print the comparison results
     console.log("Comparison Results:");
